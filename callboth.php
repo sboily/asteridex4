31c8b37feafb7491c0605abc6a4b72f5dad8db3a
Sylvain Boily <sylvainboilydroid@gmail.com>
2016-05-26 11:23:52 -0400

    add call template, fix call extension

--- callboth.php
+++ callboth.php
@@ -5,8 +5,11 @@
  SPDX-License-Identifier: GPL-3.0+
 */
 
+require("/usr/share/php/smarty3/Smarty.class.php");
 include_once("config/config.inc.php");
 include_once("lib/xivo.php");
+
+$tpl = new Smarty();
 
 $xivo = new XiVO($xivo_host);
 $xivo->xivo_api_user = $xivo_api_user;
@@ -23,46 +26,17 @@ if (strlen($OUT)>100) :
  $pos=true ;
 endif ;
 
-if ($pos===false) :
+if ($pos===false) {
+    $tpl->assign("duration", 4000);
+    $tpl->assign("exten", $OUT);
+    $tpl->assign("pos", TRUE);
+
     $xivo->do_call($OUT, $xivo_api_user, $xivo_api_pwd);
+} else {
+    $tpl->assign("duration", 1000);
+    $tpl->assign("pos", FALSE);
+}
+
+$tpl->display("tpl/call.html");
+
 ?>
-
-<html>
-  <head>
-    <script>
-      var duration = 4000;
-      x = null
-
-      function closeIt() {
-        x = setTimeout("self.close()",duration);
-      }
-    </script>
-  </head>
-
-  <body onload="closeIt();self.focus()">
-    Extension is ringing now... <br>
-    When you answer, call to <?php echo $OUT ?> will be placed.
-  </body>
-</html>
-
-<?php
-else :
-?>
-
-<html>
-  <head>
-    <script>
-      var duration = 1000;
-      x = null;
-
-      function closeIt() {
-        x = setTimeout("self.close()",duration);
-      }
-    </script>
-
-  </head>
-  <body onload="closeIt();self.focus()">
-  </body>
-</html>
-
-<?php endif; ?>
